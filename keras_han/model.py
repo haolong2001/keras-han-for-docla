02f5b00790758c987afa9c4cd65fb23ccc07c440
haolonh <e0376932@u.nus.edu>
2021-12-13 22:57:17 +0800

    git tracks changes

--- keras_han/model.py
+++ keras_han/model.py
@@ -219,6 +219,8 @@ class HAN(Model):
         print(prev_tensor.shape.as_list())
         a,b,c,d = prev_tensor.shape.as_list()
 
+
+        # example
         temp_tensor = tf.slice(prev_tensor, [0, 0, 0, 99], [16, b, c, 1])
 
         temp_tensor = tf.squeeze(temp_tensor)
@@ -229,22 +231,29 @@ class HAN(Model):
         dummy2_layer = Lambda(
             lambda x: layer._get_attention_weights(x)
         )(temp_tensor)
-        temp_result = Model(self.input, dummy2_layer).predict(X)
+        prev_tensor = Model(self.input, self.get_layer('word_encoder')).predict(X)
 
-        print(temp_result)
+
+
+        layer = AttentionLayer(name='temp')
 
         for i in range (d):
             temp_tensor = tf.slice(prev_tensor, [0, 0, 0, i], [16, b, c, 1])
             temp_tensor = tf.squeeze(temp_tensor)
-            layer = AttentionLayer(name='temp')
-            temp_sentence_rep = layer(temp_tensor)
+
             dummy2_layer = Lambda(
                 lambda x: layer._get_attention_weights(x)
             )(temp_tensor)
-            temp_result = Model(self.input, dummy2_layer).predict(X)
+
+            temp_result = Model(Input(), dummy2_layer).predict(temp_tensor) # word encoder
+
+
             ls.append(temp_result)
 
+
+
         return ls
+
 
 
 
